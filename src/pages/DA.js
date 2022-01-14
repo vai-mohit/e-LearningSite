62c18877463133c1fde1f6e9e50740bafa772d78
Kritarth Singh <ksingh2459@gmail.com>
2022-01-14 22:10:12 +0530

    0.1

--- src/pages/DA.js
+++ src/pages/DA.js
@@ -1,3 +1,30 @@
+import DApic from "./DA.jpg";
+import { Image } from "react-bootstrap";
 export default function DA() {
-  return <div>DA</div>;
+  return(
+    <div><div className="Image">
+    <Image src={DApic} fluid />
+    <div><h1>What is Data Analytics?</h1></div>
+    <div>As the process of analyzing raw data to find trends and answer questions, the definition of data analytics captures its broad scope of the field. However, it includes many techniques with many different goals.
+
+The data analytics process has some components that can help a variety of initiatives. By combining these components, a successful data analytics initiative will provide a clear picture of where you are, where you have been and where you should go.
+<br/>
+    Generally, this process begins with descriptive analytics. This is the process of describing historical trends in data. Descriptive analytics aims to answer the question “what happened?” This often involves measuring traditional indicators such as return on investment (ROI). The indicators used will be different for each industry. Descriptive analytics does not make predictions or directly inform decisions. It focuses on summarizing data in a meaningful and descriptive way.
+    The next essential part of data analytics is advanced analytics. This part of data science takes advantage of advanced tools to extract data, make predictions and discover trends. These tools include classical statistics as well as machine learning. Machine learning technologies such as neural networks, natural language processing, sentiment analysis and more enable advanced analytics. This information provides new insight from data. Advanced analytics addresses “what if?” questions.
+    The availability of machine learning techniques, massive data sets, and cheap computing power has enabled the use of these techniques in many industries. The collection of big data sets is instrumental in enabling these techniques. Big data analytics enables businesses to draw meaningful conclusions from complex and varied data sources, which has been made possible by advances in parallel processing and cheap computational power.
+</div>
+    <div><h2>Types of Data Analytics</h2></div>
+    <div>Data analytics is a broad field. There are four primary types of data analytics: descriptive, diagnostic, predictive and prescriptive analytics. Each type has a different goal and a different place in the data analysis process. These are also the primary data analytics applications in business.
+
+Descriptive analytics helps answer questions about what happened. These techniques summarize large datasets to describe outcomes to stakeholders. By developing key performance indicators (KPIs,) these strategies can help track successes or failures. Metrics such as return on investment (ROI) are used in many industries. Specialized metrics are developed to track performance in specific industries. This process requires the collection of relevant data, processing of the data, data analysis and data visualization. This process provides essential insight into past performance.
+Diagnostic analytics helps answer questions about why things happened. These techniques supplement more basic descriptive analytics. They take the findings from descriptive analytics and dig deeper to find the cause. The performance indicators are further investigated to discover why they got better or worse. This generally occurs in three steps:
+    Identify anomalies in the data. These may be unexpected changes in a metric or a particular market.
+    Data that is related to these anomalies is collected.
+    Statistical techniques are used to find relationships and trends that explain these anomalies.
+Predictive analytics helps answer questions about what will happen in the future. These techniques use historical data to identify trends and determine if they are likely to recur. Predictive analytical tools provide valuable insight into what may happen in the future and its techniques include a variety of statistical and machine learning techniques, such as: neural networks, decision trees, and regression.
+Prescriptive analytics helps answer questions about what should be done. By using insights from predictive analytics, data-driven decisions can be made. This allows businesses to make informed decisions in the face of uncertainty. Prescriptive analytics techniques rely on machine learning strategies that can find patterns in large datasets. By analyzing past decisions and events, the likelihood of different outcomes can be estimated.
+
+These types of data analytics provide the insight that businesses need to make effective and efficient decisions. Used in combination they provide a well-rounded understanding of a company’s needs and opportunities.</div>
+  </div></div>
+  );
 }
--- src/pages/IOT.js
+++ src/pages/IOT.js
@@ -1,3 +1,23 @@
+import IOTpic from "./IOT.jpg";
+import { Image } from "react-bootstrap";
 export default function IOT() {
-  return <div>IOT</div>;
+  return(
+    <div><div className="Image">
+    <Image src={IOTpic} fluid />
+    <div><h1>Information Systems</h1></div>
+    <div>The internet of things, or IoT, is a system of interrelated computing devices, mechanical and digital machines, objects, animals or people that are provided with unique identifiers (UIDs) and the ability to transfer data over a network without requiring human-to-human or human-to-computer interaction.<br/>Definition
+What is internet of things (IoT)?
+
+Alexander S. Gillis
+By
+
+    Alexander S. Gillis, Technical Writer and Editor
+
+The internet of things, or IoT, is a system of interrelated computing devices, mechanical and digital machines, objects, animals or people that are provided with unique identifiers (UIDs) and the ability to transfer data over a network without requiring human-to-human or human-to-computer interaction.
+
+A thing in the internet of things can be a person with a heart monitor implant, a farm animal with a biochip transponder, an automobile that has built-in sensors to alert the driver when tire pressure is low or any other natural or man-made object that can be assigned an Internet Protocol (IP) address and is able to transfer data over a network.<br/>Increasingly, organizations in a variety of industries are using IoT to operate more efficiently, better understand customers to deliver enhanced customer service, improve decision-making and increase the value of the business.</div>
+    <div><h2>How does IoT work?</h2></div>
+    <div>An IoT ecosystem consists of web-enabled smart devices that use embedded systems, such as processors, sensors and communication hardware, to collect, send and act on data they acquire from their environments. IoT devices share the sensor data they collect by connecting to an IoT gateway or other edge device where data is either sent to the cloud to be analyzed or analyzed locally. Sometimes, these devices communicate with other related devices and act on the information they get from one another. The devices do most of the work without human intervention, although people can interact with the devices -- for instance, to set them up, give them instructions or access the data.<br/>The connectivity, networking and communication protocols used with these web-enabled devices largely depend on the specific IoT applications deployed.<br/>IoT can also make use of artificial intelligence (AI) and machine learning to aid in making data collecting processes easier and more dynamic.</div>
+  </div></div>
+  );
 }
